a9c6116ff3f650e3469bda3ef5c0e064fd290b4d
Agjewel <je026i@gmail.com>
2015-05-16 13:52:43 +0600

    Calculate yearly cost of student

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace CalculateYearlyCostOfStudent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DiscountCheckBox = new System.Windows.Forms.CheckBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.ClassComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculate for yearly cost of a student";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select Class:";
+            // 
+            // DiscountCheckBox
+            // 
+            this.DiscountCheckBox.AutoSize = true;
+            this.DiscountCheckBox.Location = new System.Drawing.Point(50, 99);
+            this.DiscountCheckBox.Name = "DiscountCheckBox";
+            this.DiscountCheckBox.Size = new System.Drawing.Size(198, 17);
+            this.DiscountCheckBox.TabIndex = 3;
+            this.DiscountCheckBox.Text = "Get 5% discount for early admissinon";
+            this.DiscountCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(25, 175);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(232, 23);
+            this.CalculateButton.TabIndex = 4;
+            this.CalculateButton.Text = "Calculate total amount for one year";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // ClassComboBox
+            // 
+            this.ClassComboBox.FormattingEnabled = true;
+            this.ClassComboBox.Items.AddRange(new object[] {
+            "Class 1",
+            "Class 2",
+            "Class 3",
+            "Class 4",
+            "Class 6",
+            "Class 7",
+            "Class 8",
+            "Class 9",
+            "Class 10",
+            "Class 11",
+            "Class 12"});
+            this.ClassComboBox.Location = new System.Drawing.Point(96, 39);
+            this.ClassComboBox.Name = "ClassComboBox";
+            this.ClassComboBox.Size = new System.Drawing.Size(121, 21);
+            this.ClassComboBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.ClassComboBox);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.DiscountCheckBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Yearly Cost Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox DiscountCheckBox;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.ComboBox ClassComboBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateYearlyCostOfStudent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            //string firstInput = Convert.ToString(ClassComboBox.Items.Add("Class 1"));
+            //string secInput = Convert.ToString(ClassComboBox.Items.Add("Class 2"));
+
+            //string thirdInput = Convert.ToString(ClassComboBox.Items.Add("Class 3"));
+            //string forthInput = Convert.ToString(ClassComboBox.Items.Add("Class 4"));
+            //string fifthInput = Convert.ToString(ClassComboBox.Items.Add("Class 5"));
+            //string sixthInput = Convert.ToString(ClassComboBox.Items.Add("Class 6"));
+            //string seventhInput = Convert.ToString(ClassComboBox.Items.Add("Class 7"));
+            //string eightInput = Convert.ToString(ClassComboBox.Items.Add("Class 8"));
+            //string ninthInput = Convert.ToString(ClassComboBox.Items.Add("Class 9"));
+            //string tenInput = Convert.ToString(ClassComboBox.Items.Add("Class 10"));
+            //string elevenInput = Convert.ToString(ClassComboBox.Items.Add("Class 11"));
+            //string twelveInput = Convert.ToString(ClassComboBox.Items.Add("Class 12"));
+
+
+           
+            if (ClassComboBox.Text == "Class 1" && DiscountCheckBox.Checked)
+            {
+                int totalAmount =10000 + (500*12) - 500;
+                MessageBox.Show(totalAmount.ToString());
+
+            }
+            else if (ClassComboBox.Text == "Class 1" && !DiscountCheckBox.Checked)
+            {
+                int totalAmount = 10000 + (500 * 12);
+                
+                MessageBox.Show(totalAmount.ToString());
+            }
+            else if (ClassComboBox.Text == "Class 2" && DiscountCheckBox.Checked)
+            {
+                int totalAmount = 10000 + (500 * 12) -500;
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+            else if (ClassComboBox.Text == "Class 2" && !DiscountCheckBox.Checked)
+            {
+                int totalAmount = 10000 + (500 * 12);
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+
+            else if (ClassComboBox.Text == "Class 3" && DiscountCheckBox.Checked)
+            {
+                int totalAmount = 10000 + (1000 * 12) -500;
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+
+            else if (ClassComboBox.Text == "Class 3" && !DiscountCheckBox.Checked)
+            {
+                int totalAmount = 10000 + (1000 * 12);
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+
+            else if (ClassComboBox.Text == "Class 4" && DiscountCheckBox.Checked)
+            {
+                int totalAmount = 10000 + (1000 * 12) - 500;
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+
+            else if (ClassComboBox.Text == "Class 4" && !DiscountCheckBox.Checked)
+            {
+                int totalAmount = 10000 + (1000 * 12);
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+
+            else if (ClassComboBox.Text == "Class 5" && DiscountCheckBox.Checked)
+            {
+                int totalAmount = 10000 + (1000 * 2)-500;
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+
+            else if (ClassComboBox.Text == "Class 5" && !DiscountCheckBox.Checked)
+            {
+                int totalAmount = 10000 + (1000 * 12);
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+
+            else if (ClassComboBox.Text == "Class 6" && DiscountCheckBox.Checked)
+            {
+                int totalAmount = 12000 + (1500 * 12) -600;
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+
+            else if (ClassComboBox.Text == "Class 6" && !DiscountCheckBox.Checked)
+            {
+                int totalAmount = 12000 + (1500 * 12);
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+
+            else if (ClassComboBox.Text == "Class 7" && DiscountCheckBox.Checked)
+            {
+                int totalAmount = 12000 + (1500 * 12)-600;
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+
+            else if (ClassComboBox.Text == "Class 7" && !DiscountCheckBox.Checked)
+            {
+                int totalAmount = 12000 + (1500 * 12);
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+
+            else if (ClassComboBox.Text == "Class 8" && !DiscountCheckBox.Checked)
+            {
+                int totalAmount = 12000 + (1500 * 12);
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+
+            else if (ClassComboBox.Text == "Class 8" && DiscountCheckBox.Checked)
+            {
+                int totalAmount = 12000 + (1500 * 12) - 600;
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+
+            else if (ClassComboBox.Text == "Class 9" && !DiscountCheckBox.Checked)
+            {
+                int totalAmount = 12000 + (1500 * 12);
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+
+            else if (ClassComboBox.Text == "Class 9" && DiscountCheckBox.Checked)
+            {
+                int totalAmount = 15000 + (2000 * 12) - 750;
+
+                MessageBox.Show(totalAmount.ToString());
+            }
+
+
+        }
+    }
+}
